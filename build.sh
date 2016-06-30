9c178578594f15a1796aaf0cd1d0bb1026202cee
Michael R. Lawrence <mike@michaellawrenceit.com>
2016-06-30 16:51:04 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,41 +1,30 @@
 # First param is package tarball, 2nd is the *.DIGEST file
-VerifyShaOfStage3()
-{
-	test_sum=$(awk -v myvar="$1" '$2==myvar {for(i=1; i<=1; i++) { print $1; exit}}' $2)
-	calculated_sum=$(sha512sum $1 | awk '{print $1}' -)
-	if [[ "$test_sum" == "$calculated_sum" ]]; then
-		return 0
-	else
-		return 1
-	fi
-}
-
-suffix=$3 # e.g. -hardened
-arch=$1
-dist="http://distfiles.gentoo.org/releases/${arch}/autobuilds/"
-stage3path="$(wget -q -O- ${dist}/latest-stage3-${arch}${suffix}.txt | tail -n 1 | cut -f 1 -d ' ')"
-stage3="$(basename ${stage3path})"
+stage3"http://distfiles.gentoo.org/experimental/arm64/stage3-arm64-20160324.tar.bz2"
 
 # Create working directory, keep a copy of busybox handy
-mkdir newWorldOrder; cd newWorldOrder
+mkdir newWorldOrder && mkdir newWorldOrder/usr &&  mkdir /newWorldOrder/usr/bin && mkdir /newWorldOrder/etc 
+mkdir /newWorldOrder/etc/binfmt.d/
+cd newWorldOrder
 cp /bin/busybox .
-
+cat >>/newWorldOrder/etc/binfmt.d/qemu-arm64.conf <<EOF
+# AArch64 binaries.
+:qemu-arm64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/qemu-arm64:OC
+EOF
+cd /newWorldOrder/usr/bin
+wget -q -c https://github.com/necrose99/Docker-Gentoo-ARM64/raw/master/qemu-aarch64-binfmt
+cd /newWorldOrder
 echo "Downloading and extracting ${stage3path}..."
-wget -q -c "${dist}/${stage3path}" "${dist}/${stage3path}.DIGESTS"
-if VerifyShaOfStage3 $stage3 "${stage3}.DIGESTS"; then
-	echo "DIGEST sum is okey";
-else
-	echo "DIGEST sum is NOT okey";
-	return 1;
-fi
+wget -q -c "{stage3}"
+
 bunzip2 -c ${stage3} | tar --exclude "./etc/hosts" --exclude "./sys/*" -xf -
 /newWorldOrder/busybox rm -f $stage3
-
+cd /newWorldOrder
 echo "Installing stage 3"
 /newWorldOrder/busybox rm -rf /lib* /usr /var /bin /sbin /opt /mnt /media /root /home /run /tmp
 /newWorldOrder/busybox cp -fRap lib* /
 /newWorldOrder/busybox cp -fRap bin boot home media mnt opt root run sbin tmp usr var /
 /newWorldOrder/busybox cp -fRap etc/* /etc/
+
 
 # Cleaning
 cd /
@@ -45,30 +34,5 @@ cd /
 echo "Bootstrapped ${stage3path} into /:"
 ls --color -lah
 
-----working on. 
+## ----working on. 
 # Supporting crossbuilding with binfmt
-ADD ./qemu-aarch64-binfmt /usr/bin/qemu-aarch64-binfmt
-ADD ./qemu-arm64.conf-gen.sh
-RUN /bin/bash /qemu-arm64.conf-gen.sh
-RUN env-update
-RUN source /etc/profile 
-# Setup the (virtually) current runlevel
-echo "default" > /run/openrc/softlevel
-
-# Setup the rc_sys
-RUN sed -e 's/#rc_sys=""/rc_sys="lxc"/g' -i /etc/rc.conf
-
-# Setup the net.lo runlevel
-RUN ln -s /etc/init.d/net.lo /run/openrc/started/net.lo
-
-# Setup the net.eth0 runlevel
-RUN ln -s /etc/init.d/net.lo /etc/init.d/net.eth0
-RUN ln -s /etc/init.d/net.eth0 /run/openrc/started/net.eth0
-
-# By default, UTC system
-RUN echo 'UTC' > /etc/timezone
-
-
-#add more stuff via bash. and or add provisoning stub back. 
-RUN ["/bin/bash"]
-CMD ["/bin/bash"]
