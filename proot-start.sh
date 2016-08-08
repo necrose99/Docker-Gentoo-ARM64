17933b5e882dcf3566a9a54126209914039b8340
Michael R. Lawrence <mike@michaellawrenceit.com>
2016-08-08 09:59:13 -0500

    Create proot-start.sh

---
+++ proot-start.sh
@@ -0,0 +1,13 @@
+# proot-start.sh  ,
+echo "Arm64 from AMD-64 what sorcery is this ?
+echo "fire in the hole the chroot cometh"
+## do chroot and load static emulation binformat etc.. 
+## for ease of use 
+proot -R /opt/gentoo-arm64/ -q ./umeq-arm64 bash
+
+#
+#type in "uname -m" or unmae -a 
+sorcery () {
+unmae -a 
+echo " sorcery I tell you ?
+}
