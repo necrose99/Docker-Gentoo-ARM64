7834e2f4dceebbf021d977ddea29122886c61cbf
Michael R. Lawrence <mike@michaellawrenceit.com>
2016-08-08 10:02:45 -0500

    Update proot-start.sh

--- proot-start.sh
+++ proot-start.sh
@@ -4,10 +4,12 @@ echo "fire in the hole the chroot cometh"
 ## do chroot and load static emulation binformat etc.. 
 ## for ease of use 
 proot -R /opt/gentoo-arm64/ -q ./umeq-arm64 bash
-
 #
 #type in "uname -m" or unmae -a 
 sorcery () {
-unmae -a 
+uname -a && ls --color -lah 
 echo " sorcery I tell you ?
+echo "Bootstrapped  into chroot/proot  gentoo-arm64:"
+emerge-webrsync 
+# populate the portage volume with ebuilds etc. 
 }
