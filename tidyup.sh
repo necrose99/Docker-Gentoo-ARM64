3f1f1581d8a39439e85ba0db5dcdd3c44dbe2db8
Michael R. Lawrence <mike@michaellawrenceit.com>
2016-08-08 12:49:03 -0500

    Update tidyup.sh

--- tidyup.sh
+++ tidyup.sh
@@ -1,4 +1,5 @@
-rm stage3-arm64-20160324.tar.bz2
-rm portage-20160731.tar.bz2
+rm /stage3-arm64-20160324.tar.bz2
+rm /portage-20160731.tar.bz2
 ## self distruct cleaner 
-rm tidyup.sh
+rm /Dockerfile
+rm /tidyup.sh
